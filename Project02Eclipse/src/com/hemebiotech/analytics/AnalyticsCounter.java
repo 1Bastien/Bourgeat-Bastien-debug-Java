34a31c67325f3abb849cbd31cbc6b0564d22b387
Bastien <bastien.brgt@protonmail.com>
2023-10-20 23:48:22 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,46 +1,43 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
-    private static int headacheCount = 0;
-    private static int rashCount = 0;
-    private static int pupilCount = 0;
+    ISymptomReader reader;
+    ISymptomWriter writer;
 
-    public static void main(String args[]) throws Exception {
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+    
+    public List<String> getSymptoms() {
+        return reader.GetSymptoms();
+    }
 
-        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-        String line = reader.readLine();
-
-        int i = 0;
-        int headCount = 0;
-
-        while (line != null) {
-
-            i++;
-            System.out.println("symptom from file: " + line);
-
-            if (line.equals("headache")) {
-
-                headCount++;
-                System.out.println("number of headaches: " + headCount);
-            } else if (line.equals("rush")) {
-
-                rashCount++;
-            } else if (line.contains("pupils")) {
-
-                pupilCount++;
-            }
-
-            line = reader.readLine();
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> countSymptoms = new HashMap<>();
+        
+        for (String symptom : symptoms) {
+            countSymptoms.put(symptom, countSymptoms.getOrDefault(symptom, 0) + 1);
         }
-        FileWriter writer = new FileWriter("result.out");
-        writer.write("headache: " + headacheCount + "\n");
-        writer.write("rash: " + rashCount + "\n");
-        writer.write("dialated pupils: " + pupilCount + "\n");
-        writer.close();
+        
+        return countSymptoms;
+    }
+    
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+        
+        Map<String, Integer> sortSymptoms = new TreeMap<>();
+        sortSymptoms.putAll(symptoms);
+        
+        return sortSymptoms;
+    }
+    
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writer.writeSymptoms(symptoms);
     }
 }
